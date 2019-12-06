dac3b447bba2fb4ef2c77919637409fa34e018c9
robmalec <robmalec@umich.edu>
2019-12-06 13:25:32 -0500

    Finished syntax for loading election data with references to the candidate table

--- src/main/java/left/rising/FECSearchDraft/controllers/RobController.java
+++ src/main/java/left/rising/FECSearchDraft/controllers/RobController.java
@@ -2,13 +2,19 @@ package left.rising.FECSearchDraft.controllers;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.persistence.Transient;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import left.rising.FECSearchDraft.dbrepos.CandidateData;
+import left.rising.FECSearchDraft.dbrepos.CandidateDataRepo;
 import left.rising.FECSearchDraft.dbrepos.ElResult;
 import left.rising.FECSearchDraft.dbrepos.ElResultRepo;
 import left.rising.FECSearchDraft.entities.PoliticalParty;
@@ -19,24 +25,74 @@ public class RobController {
 	@Autowired
 	ElResultRepo resRepo;
 
-	static List<ElResult> tempResultStorage;
+	@Autowired
+	public CandidateDataRepo cRepo;
 
 	@RequestMapping("/load-el-data")
 	public ModelAndView loadElDataFromCSV() {
-		tempResultStorage = resRepo.findAll();
 		if (resRepo.findAll().size() == 0) {
 			BufferedReader br = null;
 			try {
+				// Loading presidential election result data
 				br = new BufferedReader(new FileReader("us-presidential-election-results.csv"));
 				String line = "";
 				line = br.readLine();
 				while ((line = br.readLine()) != null) {
-					resRepo.save(new ElResult(line));
+					String[] fields = line.split(",");
+
+					int electionYear = Integer.valueOf(fields[0]);
+
+					// Getting winning political party
+
+					PoliticalParty winningParty;
+					switch (fields[1].toLowerCase().charAt(0)) {
+					case 'd':
+						winningParty = PoliticalParty.DEMOCRAT;
+						break;
+					case 'r':
+						winningParty = PoliticalParty.REPUBLICAN;
+						break;
+					default:
+						throw new Exception("Invalid party input");
+					}
+
+					// Adding candidate names to candidates table if they aren't already there,
+					// getting relevant candidate IDs
+					String dCandName = fields[2];
+					String rCandName = fields[3];
+
+					if (cRepo.getCandidateIdFromName(rCandName).size() == 0) {
+						cRepo.save(new CandidateData(rCandName, PoliticalParty.REPUBLICAN));
+					}
+					int rCandId = cRepo.getCandidateIdFromName(rCandName).get(0);
+					if (cRepo.getCandidateIdFromName(dCandName).size() == 0) {
+						cRepo.save(new CandidateData(dCandName, PoliticalParty.DEMOCRAT));
+					}
+					int dCandId = cRepo.getCandidateIdFromName(dCandName).get(0);
+
+					resRepo.save(new ElResult(winningParty,rCandId, dCandId,electionYear));
 				}
 			} catch (Exception e) {
 				System.out.println(e.getMessage());
+				e.printStackTrace();
 			}
 		}
 		return new ModelAndView("index");
 	}
+
+	/*
+	 * Retrieves candidate ID from candidates table, adding the candidate name to
+	 * the table if it isn't already there
+	 */
+	private int getIdFromCandidateList(String cName, PoliticalParty cParty) {
+		List<CandidateData> candIDs = cRepo.findAll();
+
+		if (cRepo.getCandidateIdFromName(cName).size() == 0) {
+			cRepo.save(new CandidateData(cName, cParty));
+		}
+		return cRepo.getCandidateIdFromName(cName).get(0);
+	}
+	
+	
+
 }
