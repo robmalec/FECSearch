cae61af18c520f0c0a737513dc1a3d5c21454652
robmalec <robmalec@umich.edu>
2019-12-06 14:50:52 -0500

    More things

--- src/main/java/left/rising/FECSearchDraft/dbrepos/CandidateData.java
+++ src/main/java/left/rising/FECSearchDraft/dbrepos/CandidateData.java
@@ -10,7 +10,6 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import left.rising.FECSearchDraft.entities.CandidateCommitteeId;
 import left.rising.FECSearchDraft.entities.PoliticalParty;
 
 @Entity
