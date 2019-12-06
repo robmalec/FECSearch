e519bf8b812440095213699a61df87d5c8634f0e
robmalec <robmalec@umich.edu>
2019-12-06 09:48:53 -0500

    RobController

---
+++ src/main/java/left/rising/FECSearchDraft/controllers/RobController.java
@@ -0,0 +1,41 @@
+package left.rising.FECSearchDraft.controllers;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import left.rising.FECSearchDraft.dbrepos.ElResult;
+import left.rising.FECSearchDraft.dbrepos.ElResultRepo;
+import left.rising.FECSearchDraft.entities.PoliticalParty;
+
+@Controller
+public class RobController {
+
+	@Autowired
+	ElResultRepo resRepo;
+
+	static List<ElResult> tempResultStorage;
+
+	@RequestMapping("/load-el-data")
+	public ModelAndView loadElDataFromCSV() {
+		tempResultStorage = resRepo.findAll();
+		if (resRepo.findAll().size() == 0) {
+			BufferedReader br = null;
+			try {
+				br = new BufferedReader(new FileReader("us-presidential-election-results.csv"));
+				String line = "";
+				while ((line = br.readLine()) != null) {
+					resRepo.save(new ElResult(line));
+				}
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		return new ModelAndView("index");
+	}
+}
