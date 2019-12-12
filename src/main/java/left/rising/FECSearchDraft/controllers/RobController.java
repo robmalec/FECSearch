2b9a95e0d626b913d6a39cbf6a5944b094b8426b
robmalec <robmalec@umich.edu>
2019-12-12 10:44:12 -0500

    Pulling state-based data from API and finding biggest and smallest money winners and losers

--- src/main/java/left/rising/FECSearchDraft/controllers/RobController.java
+++ src/main/java/left/rising/FECSearchDraft/controllers/RobController.java
@@ -2,14 +2,24 @@ package left.rising.FECSearchDraft.controllers;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.google.common.util.concurrent.RateLimiter;
 
 import left.rising.FECSearchDraft.dbrepos.CanCommitteeRepo;
 import left.rising.FECSearchDraft.dbrepos.CandidateCommitteeId;
@@ -19,7 +29,9 @@ import left.rising.FECSearchDraft.dbrepos.ElResult;
 import left.rising.FECSearchDraft.dbrepos.ElResultRepo;
 import left.rising.FECSearchDraft.dbrepos.State;
 import left.rising.FECSearchDraft.dbrepos.StateRepo;
+import left.rising.FECSearchDraft.entities.DBDonationResult;
 import left.rising.FECSearchDraft.entities.PoliticalParty;
+import left.rising.FECSearchDraft.entities.StateScheduleAResults;
 
 @Controller
 public class RobController {
@@ -29,19 +41,24 @@ public class RobController {
 
 	@Autowired
 	CandidateDataRepo cRepo;
-	
+
 	@Autowired
 	CanCommitteeRepo canComRepo;
-	
-	@Autowired 
+
+	@Autowired
 	StateRepo sRepo;
-	
+
 	@Value("${gmaps.key}")
 	String gMapsKey;
-	
+
+	@Value("${fec.key}")
+	String fecKey;
+
 	List<State> states;
-	
+
 	RestTemplate rt = new RestTemplate();
+
+	final RateLimiter rateLimiter = RateLimiter.create(2.0);
 
 	@RequestMapping("/load-el-data")
 	public ModelAndView loadElDataFromCSV() {
@@ -86,29 +103,29 @@ public class RobController {
 					}
 					int dCandId = cRepo.getCandidateIdFromName(dCandName).get(0);
 
-					resRepo.save(new ElResult(winningParty,rCandId, dCandId,electionYear));
+					resRepo.save(new ElResult(winningParty, rCandId, dCandId, electionYear));
 				}
-				
+
 				br.close();
-				//Loading committee IDs
+				// Loading committee IDs
 				br = new BufferedReader(new FileReader("committee_ids.csv"));
 				line = br.readLine();
 				while ((line = br.readLine()) != null) {
 					fields = line.split(",");
-					
+
 					System.out.println(fields[0]);
-					
+
 					int year = Integer.valueOf(fields[0]);
 					CandidateData candidate = cRepo.getCandidateDataFromName(fields[1]).get(0);
-					
+
 					String committeeId = fields[2];
-					
+
 					System.out.println(line);
-					
+
 					canComRepo.save(new CandidateCommitteeId(year, candidate, committeeId));
 				}
 				br.close();
-				
+
 			} catch (Exception e) {
 				System.out.println(e.getMessage());
 				e.printStackTrace();
@@ -116,35 +133,124 @@ public class RobController {
 		}
 		return new ModelAndView("index");
 	}
-	
+
 	@RequestMapping("load-test-map")
 	public ModelAndView loadMap() {
-		ModelAndView view = new ModelAndView("test-map","apiKey",gMapsKey);
-		
+		ModelAndView view = new ModelAndView("test-map", "apiKey", gMapsKey);
+
 		view.addObject("opacity", 0.9);
-		
+
 		states = sRepo.findAll();
-		
+
 		String idString = "";
 		String stateCodeString = "";
-		
+
 		for (State s : states) {
 			idString += s.getOSMStateId() + " ";
 			stateCodeString += s.getStateCode() + " ";
 		}
-		
+
 		view.addObject("idString", idString);
 		view.addObject("stateCodes", stateCodeString);
-		
+
 		return view;
 	}
-	
+
 	@RequestMapping("load-state-stats-page")
-	public ModelAndView loadStateStatsPage(String stateCode){
-		/*
-		 * TODO: Run statewide query and send returned data to a new  ModelAndView
-		 */
-		return new ModelAndView("state-stats-page");
-		
+	public ModelAndView loadStateStatsPage(String stateCode) {
+		ModelAndView mv = new ModelAndView("state-stats-page");
+		HttpEntity<String> httpEnt = new HttpEntity<>("parameters", getHeaders());
+		ResponseEntity<StateScheduleAResults> respEnt;
+		ArrayList<StateScheduleAResults> resultList = new ArrayList<>();
+
+		List<CandidateCommitteeId> comIDList = canComRepo.findAll();
+		List<ElResult> elResultList = resRepo.findAll();
+
+		Map<CandidateData, Double> fundingMap = new HashMap<>();
+
+		double totalWinCandFundsDonated = 0.0;
+		double totalLoseCandFundsDonated = 0.0;
+		double totalFundsDonated = 0.0;
+
+		CandidateData biggestMoneyWinner = null;
+		double bmwBudget = 0.0;
+
+		CandidateData biggestMoneyLoser = null;
+		double bmlBudget = 0.0;
+
+		CandidateData smallestMoneyWinner = null;
+		double smwBudget = Double.MAX_VALUE;
+
+		CandidateData smallestMoneyLoser = null;
+		double smlBudget = Double.MAX_VALUE;
+
+		String url = "";
+
+		for (CandidateCommitteeId c : comIDList) {
+			url = "http://api.open.fec.gov/v1/schedules/schedule_a/by_state/?api_key=" + fecKey + "&committee_id="
+					+ c.getCommittee_id() + "&state=" + stateCode + "&per_page=100";
+			System.out.println(url);
+			respEnt = rt.exchange(url, HttpMethod.GET, httpEnt, StateScheduleAResults.class);
+
+			rateLimiter.acquire();
+			StateScheduleAResults results = rt.getForObject(url, StateScheduleAResults.class);
+
+			resultList.add(results);
+
+			CandidateData thisCand = c.getCandidate_assigned();
+
+			for (DBDonationResult r : results.getResults()) {
+				if ((r.getCycle() % 4 == 0) && r.getCycle() < 2020) {
+
+					ElResult thisResult = resRepo.findByElectionYear(r.getCycle()).get(0);
+
+					double fundsDonated = r.getTotal();
+
+					totalFundsDonated += fundsDonated;
+
+					if (thisCand.getAfiliatedParty() == thisResult.getWinningParty()) {
+						// Code for if committee c donated to a winning candidate this cycle
+						totalWinCandFundsDonated += fundsDonated;
+						if (fundsDonated > bmwBudget) {
+							biggestMoneyWinner = thisCand;
+						} else if (fundsDonated < smwBudget) {
+							smallestMoneyWinner = thisCand;
+						}
+					} else {
+						// Code for if committee c donated to a losing candidate this cycle
+						totalLoseCandFundsDonated += fundsDonated;
+						if (fundsDonated > bmlBudget) {
+							biggestMoneyLoser = thisCand;
+						}
+						else if (fundsDonated < smlBudget) {
+							smallestMoneyLoser = thisCand;
+						}
+					}
+				}
+			}
+		}
+
+		mv.addObject("totalWinningFunds", totalWinCandFundsDonated);
+		mv.addObject("totalLosingFunds", totalLoseCandFundsDonated);
+		mv.addObject("totalFunds", totalFundsDonated);
+		mv.addObject("bmw", biggestMoneyWinner);
+		mv.addObject("bmwBudget", bmwBudget);
+		mv.addObject("bml", biggestMoneyLoser);
+		mv.addObject("bmlBudget",bmlBudget);
+		mv.addObject("smw", smallestMoneyWinner);
+		mv.addObject("smwBudget", smwBudget);
+		mv.addObject("sml", smallestMoneyLoser);
+		mv.addObject("smlBudget", smlBudget);
+
+		return mv;
+	}
+
+	public HttpHeaders getHeaders() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(HttpHeaders.USER_AGENT, "testing");
+		headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+		headers.add("api_key", fecKey);
+		headers.add("location", "Detroit+MI");
+		return headers;
 	}
 }
