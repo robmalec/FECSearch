a4297e35f61f3c1b71120778e88077faad6d22dc
robmalec <robmalec@umich.edu>
2019-12-17 14:24:36 -0500

    Fixed the code showing state search results

--- src/main/java/left/rising/FECSearchDraft/controllers/RobController.java
+++ src/main/java/left/rising/FECSearchDraft/controllers/RobController.java
@@ -5,8 +5,10 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,12 +25,12 @@ import org.springframework.web.servlet.ModelAndView;
 import com.google.common.util.concurrent.RateLimiter;
 
 import left.rising.FECSearchDraft.dbrepos.CanCommitteeRepo;
+import left.rising.FECSearchDraft.dbrepos.CanFundsPerStateRepo;
 import left.rising.FECSearchDraft.dbrepos.CandidateCommitteeId;
 import left.rising.FECSearchDraft.dbrepos.CandidateData;
 import left.rising.FECSearchDraft.dbrepos.CandidateDataRepo;
 import left.rising.FECSearchDraft.dbrepos.ElResult;
 import left.rising.FECSearchDraft.dbrepos.ElResultRepo;
-import left.rising.FECSearchDraft.dbrepos.PerStateCandDataRepo;
 import left.rising.FECSearchDraft.dbrepos.State;
 import left.rising.FECSearchDraft.dbrepos.StateRepo;
 import left.rising.FECSearchDraft.entities.CandFundsPerState;
@@ -49,10 +51,10 @@ public class RobController {
 	CanCommitteeRepo canComRepo;
 
 	@Autowired
-	StateRepo sRepo;
-	
+	CanFundsPerStateRepo cfpsRepo;
+
 	@Autowired
-	PerStateCandDataRepo pscdRepo;
+	StateRepo sRepo;
 
 	@Value("${gmaps.key}")
 	String gMapsKey;
@@ -60,6 +62,16 @@ public class RobController {
 	@Value("${fec.key}")
 	String fecKey;
 
+	@Value("${fec.key2}")
+	String fecKey2;
+	
+	@Value("${fec.key3}")
+	String fecKey3;
+	
+	@Value("${fec.key4}")
+	String fecKey4;
+	
+	ArrayList<String> fecKeys;
 	List<State> states;
 
 	RestTemplate rt = new RestTemplate();
@@ -164,152 +176,227 @@ public class RobController {
 
 	@RequestMapping("load-state-stats-page")
 	public ModelAndView loadStateStatsPage(String stateCode) {
+		fecKeys = new ArrayList<>();
+		fecKeys.add(fecKey);
+		fecKeys.add(fecKey2);
+		fecKeys.add(fecKey3);
+		fecKeys.add(fecKey4);
+
 		ModelAndView mv = new ModelAndView("state-stats-page");
-		HttpEntity<String> httpEnt = new HttpEntity<>("parameters", getHeaders());
-		ResponseEntity<StateScheduleAResults> respEnt;
-		ArrayList<StateScheduleAResults> resultList = new ArrayList<>();
-		ArrayList<CandFundsPerState> candFunds = new ArrayList<>();
-		
-		/*
-		 * NOTE: I had to rewrite this as a list of object arrays. 
-		 * Also had to rewrite CandFundsPerState to include an integer reference to the row in the CandidateData table that contains the candidate we're looking at,
-		 * rather than having a reference to the CandidateData class that was initially there, because saving the state data to MySQL was getting very messy and JpaRepository
-		 * was not liking the fact that we had a table referencing a table that references yet another table.  So I changed it to an integer reference in order to get the 
-		 * code to compile.  Changed the former candFunds to an ArrayList of object arrays because changing this CandFundsPerState reference to an integer meant I could no longer
-		 * get the candidate name in the same way I was doing before, and I formerly learned that working with JpaRepos inside of classes was just about impossible, so I did this 
-		 * to work around having to do that.  Please slack me if you have any questions.
-		 */
-		
-		ArrayList<Object[]> candFundsArr = new ArrayList<>();
+		List<CandFundsPerState> fundsFromThisState = new ArrayList<>(); //cfpsRepo.findByStateCode(stateCode);
 
-		List<CandidateCommitteeId> comIDList = canComRepo.findAll();
-		List<ElResult> elResultList = resRepo.findAll();
+		// Testing: printing committee IDs and their associated candidates
+		for (CandidateCommitteeId c : canComRepo.findAll()) {
+			System.out.println("Committee ID: " + c.getId() + " Associated candidate: "
+					+ c.getCandidate_assigned().getName() + " in year: " + c.getElection_year());
+		}
 
-		Map<CandidateData, Double> fundingMap = new HashMap<>();
+		if (fundsFromThisState.size() == 0) {
+			// Running this code if there isn't any per-state search data saved in our DB
+			HttpEntity<String> httpEnt = new HttpEntity<>("parameters", getHeaders());
+			ResponseEntity<StateScheduleAResults> respEnt;
 
-		double totalWinCandFundsDonated = 0.0;
-		double totalLoseCandFundsDonated = 0.0;
-		double totalFundsDonated = 0.0;
+			/*
+			 * NOTE: I had to rewrite this as a list of object arrays. Also had to rewrite
+			 * CandFundsPerState to include an integer reference to the row in the
+			 * CandidateData table that contains the candidate we're looking at, rather than
+			 * having a reference to the CandidateData class that was initially there,
+			 * because saving the state data to MySQL was getting very messy and
+			 * JpaRepository was not liking the fact that we had a table referencing a table
+			 * that references yet another table. So I changed it to an integer reference in
+			 * order to get the code to compile. Changed the former candFunds to an
+			 * ArrayList of object arrays because changing this CandFundsPerState reference
+			 * to an integer meant I no longer get the candidate name in the same way I was
+			 * doing before, and I formerly learned that working with JpaRepos inside of
+			 * classes was just about impossible, so I did this to work around having to do
+			 * that. Please slack me if you have any questions.
+			 */
 
-		CandFundsPerState bmw = new CandFundsPerState(-1,0.0);
+			List<CandidateCommitteeId> comIDList = canComRepo.findAll();
+			List<ElResult> elResultList = resRepo.findAll();
 
-		CandFundsPerState bml = new CandFundsPerState(-1,0.0);
+			Map<CandidateData, Double> fundingMap = new HashMap<>();
 
-		CandFundsPerState smw = new CandFundsPerState(-1, Double.MAX_VALUE);
+			String url = "";
 
-		CandFundsPerState sml = new CandFundsPerState(-1, Double.MAX_VALUE);
+			String stateName = sRepo.findByStateCode(stateCode).get(0).getStateName();
+			mv.addObject("stateName", stateName);
+			
+			StateScheduleAResults results = null;
 
-		String url = "";
-		
-		String stateName = sRepo.findByStateCode(stateCode).get(0).getStateName();
-		mv.addObject("stateName",stateName);
-
-		for (CandidateCommitteeId c : comIDList) {
-			url = "http://api.open.fec.gov/v1/schedules/schedule_a/by_state/?api_key=" + fecKey + "&committee_id="
-					+ c.getCommittee_id() + "&state=" + stateCode + "&per_page=100";
-			System.out.println(url);
-			respEnt = rt.exchange(url, HttpMethod.GET, httpEnt, StateScheduleAResults.class);
-
-			rateLimiter.acquire();
-			StateScheduleAResults results = rt.getForObject(url, StateScheduleAResults.class);
-
-			resultList.add(results);
-
-			CandidateData thisCand = c.getCandidate_assigned();
-
-			for (DBDonationResult r : results.getResults()) {
-				if ((r.getCycle() % 4 == 0) && r.getCycle() < 2020 && r.getCycle() >= 1980) {
-
-					ElResult thisResult = resRepo.findByElectionYear(r.getCycle()).get(0);
-
-					double fundsDonated = r.getTotal();
-
-					totalFundsDonated += fundsDonated;
-					
-					CandFundsPerState thisCandFunds = new CandFundsPerState(thisCand.getId(), fundsDonated);
-					
-					if ((candFunds.isEmpty()) || (fundsDonated <= candFunds.get(0).getFunds())) {
-						candFundsArr.add(0,getCandidateInfo(thisCand.getId(), fundsDonated));
+			// going through our list of candidate committee IDs
+			for (CandidateCommitteeId c : comIDList) {
+				
+				for (String key : fecKeys) {
+					try {
+					url = "http://api.open.fec.gov/v1/schedules/schedule_a/by_state/?api_key=" + key + "&committee_id="
+							+ c.getCommittee_id() + "&state=" + stateCode + "&per_page=100";
+					results = rt.getForObject(url, StateScheduleAResults.class);
+					break;
 					}
-					else if (fundsDonated >= candFunds.get(candFunds.size() - 1).getFunds()) {
-						candFundsArr.add(getCandidateInfo(thisCand.getId(), fundsDonated));
+					catch (Exception e) {
+						
 					}
-					else {
-						/*candFunds.add(thisCandFunds);						
-						candFunds.sort(new Comparator<CandFundsPerState>() {
-							@Override
-							public int compare(CandFundsPerState lhs, CandFundsPerState rhs) {
-								if (lhs.getFunds() <= rhs.getFunds()) {
-									return 1;
-								}
-								else{
-									return -1;
-								}
-							}							
-						});*/
-						candFundsArr.add(getCandidateInfo(thisCand.getId(), fundsDonated));
-						candFundsArr.sort(new Comparator<Object[]>() {
-							public int compare(Object[] lhs, Object[] rhs) {
-								if ((double)lhs[1] <= (double)rhs[1]) {
-									return 1;
-								}
-								else {
-									return -1;
-								}
+				}
+				
+
+				String candName = c.getCandidate_assigned().getName();
+
+				CandidateData thisCand = c.getCandidate_assigned();
+
+				// Going through each result
+				for (DBDonationResult r : results.getResults()) {
+
+					
+
+					Boolean resultIsValidElectionYear = (((r.getCycle() % 4) == 0) && (r.getCycle() >= 1980)
+							&& (r.getCycle() < 2020));
+
+					if (resultIsValidElectionYear) {
+						ElResult thisResult = getResultOfYear(r.getCycle());
+						
+						
+						
+						Boolean candidateIsWinnerOrLoser = ((thisCand.getId() == thisResult.getWinnerId())
+								|| (thisCand.getId() == thisResult.getLoserId()));
+						if (candidateIsWinnerOrLoser) {
+							System.out.println("Year: " + r.getCycle());
+							System.out.println("Candidate name: " + getCandidateData(thisCand.getId()).getName());
+
+
+							int posOfCandInList = listContainsCandidateInYear(fundsFromThisState, thisCand.getId(),
+									r.getCycle());
+							if (posOfCandInList == -1) {
+								CandFundsPerState newCFPS = new CandFundsPerState(stateCode, thisCand.getId(),
+										r.getCycle(), r.getTotal());
+								fundsFromThisState.add(newCFPS);
+								
+							} else {
+								fundsFromThisState.get(posOfCandInList).addFunds(r.getTotal());
 							}
-						});
-
-					}
-
-					if (thisCand.getAfiliatedParty() == thisResult.getWinningParty()) {
-						// Code for if committee c donated to a winning candidate this cycle
-						totalWinCandFundsDonated += fundsDonated;
-						if (fundsDonated > bmw.getFunds()) {
-							bmw = thisCandFunds;
-						} else if (fundsDonated < smw.getFunds()) {
-							smw = thisCandFunds;
-						}
-					} else {
-						// Code for if committee c donated to a losing candidate this cycle
-						totalLoseCandFundsDonated += fundsDonated;
-						if (fundsDonated > bml.getFunds()) {
-							bml = thisCandFunds;
-						}
-						else if (fundsDonated < sml.getFunds()) {
-							sml = thisCandFunds;
 						}
 					}
 				}
 			}
+			
+			// Finally saving everything to the DB
+			System.out.println("Contents of fundsFromThisState");
+			for (CandFundsPerState c : fundsFromThisState) {
+				System.out.println(c);
+				//cfpsRepo.save(c);
+			}
 		}
-		System.out.println(candFunds);
+
+		// Calculating biggest and smallest money winners and losers from list
+
+		CandFundsPerState bmw = new CandFundsPerState(stateCode, -1, -1, 0.0);
+		CandFundsPerState bml = new CandFundsPerState(stateCode, -1, -1, 0.0);
+		CandFundsPerState smw = new CandFundsPerState(stateCode, -1, -1, Double.MAX_VALUE);
+		CandFundsPerState sml = new CandFundsPerState(stateCode, -1, -1, Double.MAX_VALUE);
+
+		Double totalWinningFunds = 0.0;
+		Double totalLosingFunds = 0.0;
+		Double totalFunds = 0.0;
+
+		Double thisCandFunds = 0.0;
+		for (CandFundsPerState c : fundsFromThisState) {
+			thisCandFunds = c.getFunds();
+			totalFunds += thisCandFunds;
+			String thisCandName = getCandidateData(c.getCandId()).getName();
+			if (thisCandName.equals("Walter Mondale")) {
+				int pig = 0;
+			}
+
+			ElResult result = getResultOfYear(c.getYear());
+
+			// Determine if candidate is a winner or loser
+			if (c.getCandId() == result.getWinnerId()) {
+				totalWinningFunds += thisCandFunds;
+				if (thisCandFunds > bmw.getFunds()) {
+					bmw = c;
+				} else if (thisCandFunds < smw.getFunds()) {
+					smw = c;
+				}
+			} else if (c.getCandId() == result.getLoserId()) {
+				totalLosingFunds += thisCandFunds;
+				if (thisCandFunds > bml.getFunds()) {
+					bml = c;
+				} else if (thisCandFunds < sml.getFunds()) {
+					sml = c;
+				}
+			}
+		}
+
+		fundsFromThisState.sort(new Comparator<CandFundsPerState>() {
+			public int compare(CandFundsPerState lhs, CandFundsPerState rhs) {
+				if (lhs.getFunds() <= rhs.getFunds()) {
+					return 1;
+				} else {
+					return -1;
+				}
+			}
+		});
+
+		ArrayList<Object[]> candFundsArr = new ArrayList<>();
+		for (CandFundsPerState c : fundsFromThisState) {
+			candFundsArr.add(getCandidateInfo(c.getCandId(), c.getFunds(), c.getYear()));
+		}
+
+		// Sorting array of per-candidate state fundraising numbers
 
 		mv.addObject("candFundsList", candFundsArr);
-		mv.addObject("totalWinningFunds", totalWinCandFundsDonated);
-		mv.addObject("totalLosingFunds", totalLoseCandFundsDonated);
-		mv.addObject("totalFunds", totalFundsDonated);
-		
+		mv.addObject("totalWinningFunds", removeScientificNotation(totalWinningFunds));
+		mv.addObject("totalLosingFunds", removeScientificNotation(totalLosingFunds));
+		mv.addObject("totalFunds", removeScientificNotation(totalFunds));
+
 		mv.addObject("bmw", getCandidateData(bmw.getCandId()));
-		mv.addObject("bmwBudget", bmw.getFunds());
+		mv.addObject("bmwBudget", removeScientificNotation(bmw.getFunds()));
 
 		mv.addObject("bml", getCandidateData(bml.getCandId()));
-		mv.addObject("bmlBudget", bml.getFunds());
-		
+		mv.addObject("bmlBudget", removeScientificNotation(bml.getFunds()));
+
 		mv.addObject("smw", getCandidateData(smw.getCandId()));
-		mv.addObject("smwBudget", smw.getFunds());
-		
+		mv.addObject("smwBudget", removeScientificNotation(smw.getFunds()));
+
 		mv.addObject("sml", getCandidateData(sml.getCandId()));
-		mv.addObject("smlBudget", sml.getFunds());
+		mv.addObject("smlBudget", removeScientificNotation(sml.getFunds()));
 
 		return mv;
 	}
 
-	public Object[] getCandidateInfo(int candId, double budget) {
-		return new Object[] {getCandidateData(candId).getName(),budget};
+	public int listContainsCandidateInYear(List<CandFundsPerState> list, int candId, int year) {
+		int i = 0;
+		for (CandFundsPerState c : list) {
+			if ((c.getCandId() == candId) && (c.getYear() == year)) {
+				return i;
+			}
+			i++;
+		}
+		return -1;
 	}
+
+	ElResult getResultOfYear(int year) {
+		try {
+			return resRepo.findByElectionYear(year).get(0);
+		} catch (Exception e) {
+			System.out.println("No results for year " + year);
+			return null;
+		}
+	}
+
+	public String removeScientificNotation(double input) {
+		String output = String.format("%20.0f", input);
+		return output;
+	}
+
+	public Object[] getCandidateInfo(int candId, double budget, int year) {
+		return new Object[] { getCandidateData(candId).getName(), removeScientificNotation(budget), year };
+	}
+
 	public CandidateData getCandidateData(int candId) {
 		return cRepo.findById(candId).orElse(null);
 	}
+
 	public HttpHeaders getHeaders() {
 		HttpHeaders headers = new HttpHeaders();
 		headers.add(HttpHeaders.USER_AGENT, "testing");
