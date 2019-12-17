6009c619d9f7b8db1d077b90c37830cbb5ce6bc3
Robert Malecki <robmalec@umich.edu>
2019-12-17 14:51:16 -0500

    Delete PerStateFundraisingDataRepo.java

--- src/main/java/left/rising/FECSearchDraft/dbrepos/PerStateFundraisingDataRepo.java
+++
@@ -1,9 +0,0 @@
-package left.rising.FECSearchDraft.dbrepos;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PerStateCandDataRepo extends JpaRepository<PerStateCandData, Integer>{
-	public List<PerStateCandData> findByStateCode(String code);
-}
